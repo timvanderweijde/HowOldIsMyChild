fcf7eea22aa605e93e5ce291db51c36886cc965e
Tim van der Weijde <tim@bluecape.nl>
2015-03-04 10:21:02 +0100

    first draft

---
+++ UniversalApp/UniversalApp.Shared/ViewModels/ChildViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UniversalApp.ViewModels
+{
+    public class ChildViewModel
+    {
+        public ChildViewModel(string name, DateTime birthDate)
+        {
+            Name = name;
+            BirthDate = birthDate;
+        }
+
+        public string Name
+        {
+            get;
+            private set;
+        }
+
+        public DateTime BirthDate
+        {
+            get;
+            private set;
+        }
+    }
+}
---
+++ UniversalApp/UniversalApp.Shared/ViewModels/MainViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Popups;
+using Caliburn.Micro;
+
+namespace UniversalApp.ViewModels
+{
+    public class MainViewModel : Screen
+    {
+        public MainViewModel()
+        {
+            Childs = new BindableCollection<ChildViewModel>
+            {
+                new ChildViewModel("Daan van der Weijde", new DateTime(2011, 10, 30)),
+                new ChildViewModel("Lukas van der Weijde", new DateTime(2014, 2, 25)),
+            };
+        }
+
+        public BindableCollection<ChildViewModel> Childs
+        {
+            get;
+            private set;
+        }
+
+        public async void ChildSelected(ChildViewModel child)
+        {
+            var dialog = new MessageDialog(String.Format("{0} selected.", child.Name), "Child Selected");
+
+            await dialog.ShowAsync();
+        }
+    }
+}
